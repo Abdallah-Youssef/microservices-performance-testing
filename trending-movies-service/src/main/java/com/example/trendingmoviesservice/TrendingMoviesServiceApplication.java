3829fe179781f489c422598ba47c43899dcf1f38
Abdallah <abdallah.yossf@gmail.com>
2023-03-11 20:22:19 +0200

    RPC server

--- trending-movies-service/src/main/java/com/example/trendingmoviesservice/TrendingMoviesServiceApplication.java
+++ trending-movies-service/src/main/java/com/example/trendingmoviesservice/TrendingMoviesServiceApplication.java
@@ -4,14 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
 public class TrendingMoviesServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TrendingMoviesServiceApplication.class, args);
-    }
+
+  public static void main(String[] args) {
+    SpringApplication.run(TrendingMoviesServiceApplication.class, args);
+  }
 
 }
--- trending-movies-service/src/main/java/com/example/trendingmoviesservice/services/MyServiceImpl.java
+++ trending-movies-service/src/main/java/com/example/trendingmoviesservice/services/MyServiceImpl.java
@@ -2,38 +2,28 @@ package com.example.trendingmoviesservice.services;
 
 import com.hambola.*;
 import com.hambola.TrendingMovies.Builder;
-import com.hambola.TrendingMovies.Movie;
 
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @GrpcService
 public class MyServiceImpl extends MyServiceGrpc.MyServiceImplBase {
 
-  private RestTemplate restTemplate;
-
   @Override
   public void getTrendingMovies(com.hambola.Empty request,
       io.grpc.stub.StreamObserver<com.hambola.TrendingMovies> responseObserver) {
-        
-    Movie movie1 = Movie.newBuilder().setMovieId("Movie 1").setRating((float) 9.5).build();
-    Movie movie2 = Movie.newBuilder().setMovieId("Movie 2").setRating((float) 5.9).build();
-    Movie movie3 = Movie.newBuilder().setMovieId("Movie 3").setRating((float) 4.2).build();
 
-    final String url = "https://ratings-data-service/3/movie/" + movieId + "?api_key=" + apiKey;
-    MovieSummary movieSummary = restTemplate.getForObject(url, MovieSummary.class);
+    final String url = "http://localhost:8083/ratings/top/";
+    final RestTemplate restTemplate = new RestTemplate();
 
-    return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
-
-    Movie[] movies = new Movie[]{
-      movie1,
-      movie2,
-      movie3
-    };
+    String[] l = restTemplate.getForObject(url, String[].class);
 
     Builder trendingMovies = TrendingMovies.newBuilder();
-    for (Movie movie: movies) {
-      trendingMovies.addMovies(movie);
+    for (String movie : l) {
+      trendingMovies.addMovieId(movie);
     }
 
     responseObserver.onNext(trendingMovies.build());
